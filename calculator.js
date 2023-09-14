827ad229040f44486763921daada7798c7bfc0cd
Anceo93 <144989256+Anceo93@users.noreply.github.com>
2023-09-14 12:04:43 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,16 @@
+let container = document.querySelector("container");
+//let isShow = true;
+//function container(){
+//    if (isShow) {
+//        container.style.display = "none";
+//        isShow = false;    
+//    } else{
+//       container.style.display = "block";
+//       isShow = true;
+//    }
+//}
+
+function lets(){
+    container.classlist.toggle ("hide");
+
+}
